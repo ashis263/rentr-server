e970e5c4ff610fb18c08b7fda3c51571c2925eba
Ashis Kumar Joy <ashis263@gmail.com>
2024-12-29 14:11:48 +0600

    added readme and updated for deployment

---
+++ README.md
@@ -0,0 +1,19 @@
+# Project: Rentr-server
+Rentr is a car rental website where user can browse, rent, add and manage cars and bookings. It is developed using reactJS, nodeJS, expressJS and mongoDB. Authentication made with firebase. Private API is secured with jsonwebtoken and axios interceptor.
+
+## Purpose
+This website is developed to implement the learnings of developing a full stack website using react, node, express, mongodb, firebase and jsonwebtoken.
+
+## Key features of this project
+-  Users can browse cars after they have been landed this website from home and available car page. Also there will be latest cars in the homepage.
+
+-  After login they can be able add, view and manage them.
+
+-  They can also book car for renting, after booking they will be able to manage them.
+
+## NPM packages used
+animate.css
+, axios, firebase, lottie-react, moment, motion, react, react-dom, react-dropzone, react-helmet-async, react-icons, react-responsive-carousel, react-responsive-modal, react-router, react-router-dom, react-simple-typewriter, react-tooltip, recharts, sweetalert2
+ ## Live Links for this project
+- [On Firebase](https://rentr-ashky.web.app/)
+- [On Netlify](https://rentr-ashis263.netlify.app/)
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
-require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId, Int32 } = require('mongodb');
 
 const app = express();
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 
 //middlewares
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://rentr-ashis263.netlify.app', 'https://rentr-ashky.web.app'],
   credentials: true
 }));
 app.use(express.json());
@@ -50,7 +50,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const userCollection = client.db("rentrDB").collection("users");
     const carCollection = client.db("rentrDB").collection("cars");
@@ -243,8 +243,8 @@ async function run() {
     })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
